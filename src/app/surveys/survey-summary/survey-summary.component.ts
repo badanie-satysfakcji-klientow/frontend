bd37a9ad00f8a4347f6efb8ba49b8356736686c6
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-04-25 15:08:07 +0200

    Formatting dates

--- src/app/surveys/survey-summary/survey-summary.component.ts
+++ src/app/surveys/survey-summary/survey-summary.component.ts
@@ -10,7 +10,26 @@ import {Input} from "@angular/core";
 export class SurveySummaryComponent {
   @Input() surveyConfiguration!: SurveyConfiguration;
 
-  constructor() {
+  getStartEndDateFormatted() {
+    const {startDate, endDate} = this.surveyConfiguration;
+    return `${SurveySummaryComponent.getDateFormatted(startDate)} - ${SurveySummaryComponent.getDateFormatted(endDate)}`
+  }
+
+  private static getDateFormatted(dateRepresentation: string) {
+    const date = new Date(Date.parse(dateRepresentation));
+    const dateString = date.toLocaleDateString().length < 10 ? `0${date.toLocaleDateString()}` : `${date.toLocaleDateString()}`;
+
+    return `${dateString}, godz. ${SurveySummaryComponent.getHourMinutesFormatted(date)}`;
+  }
+
+  private static getHourMinutesFormatted(date: Date) {
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    return `${SurveySummaryComponent.getTimeFormatted(hours)}:${SurveySummaryComponent.getTimeFormatted(minutes)}`;
+  }
+
+  private static getTimeFormatted(value: number) {
+    return `${value < 10 ? '0' : ''}${value}`;
   }
 
 }
