9b262154ab7ef858da8cd3e995e0dd1d8abad7e9
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-08 01:50:58 +0200

    Displaying recipients

--- src/app/send-surveys/send-surveys/send-surveys.component.ts
+++ src/app/send-surveys/send-surveys/send-surveys.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {SurveysService} from "../services/surveys.service";
 import {DropdownItem} from "../../shared/interfaces/dropdown-item";
-import {FormArray, FormBuilder} from "@angular/forms";
+import {FormBuilder} from "@angular/forms";
 import {Validators} from "@angular/forms";
 import {SendFormGroup} from "../interfaces/send-form-group";
 
