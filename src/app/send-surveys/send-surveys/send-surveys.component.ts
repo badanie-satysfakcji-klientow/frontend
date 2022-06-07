13a9a79203a956b24103a5b41517a0c92bfe26c0
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-08 01:34:37 +0200

    Code refactoring

--- src/app/send-surveys/send-surveys/send-surveys.component.ts
+++ src/app/send-surveys/send-surveys/send-surveys.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 import {SurveysService} from "../services/surveys.service";
 import {DropdownItem} from "../../shared/interfaces/dropdown-item";
-import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder} from "@angular/forms";
 import {Validators} from "@angular/forms";
+import {SendFormGroup} from "../interfaces/send-form-group";
 
 @Component({
   selector: 'app-send-surveys',
@@ -12,7 +13,7 @@ import {Validators} from "@angular/forms";
 export class SendSurveysComponent {
   creatorId: string;
   items: DropdownItem[];
-  sendForm: FormGroup;
+  sendForm: SendFormGroup
 
   constructor(private surveys: SurveysService,
               private formBuilder: FormBuilder
@@ -25,16 +26,18 @@ export class SendSurveysComponent {
     this.sendForm = formBuilder.group({
       surveyId: formBuilder.control('', {validators: [Validators.required]}),
       recipients: formBuilder.array([], {validators: [Validators.required]}),
-      recipient: formBuilder.control('', {validators: [Validators.required, Validators.email]})
-    })
+      recipient: formBuilder.control('', {
+        validators: [Validators.required, Validators.email],
+        initialValueIsDefault: true
+      })
+    }) as SendFormGroup;
   }
 
   onAddClick() {
-    (this.sendForm.controls['recipients'] as FormArray)
-      .push(this.formBuilder.control(
-        this.sendForm.value['recipient'],
-        {validators: [Validators.required, Validators.email]})
-      );
+    const validators = [Validators.required, Validators.email]
+    this.sendForm.controls.recipients
+      .push(this.formBuilder.control(this.sendForm.value.recipient, {validators: validators}));
+    this.sendForm.controls.recipient.reset();
   }
 
 }
