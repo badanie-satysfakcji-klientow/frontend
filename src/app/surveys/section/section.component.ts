85a71a22c971f2195edb33c06613f159d7750e1d
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-17 23:15:59 +0200

    Display remove section button

--- src/app/surveys/section/section.component.ts
+++ src/app/surveys/section/section.component.ts
@@ -1,14 +1,25 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, SimpleChanges} from '@angular/core';
 import {SectionFormGroup} from "../interfaces/section-form-group";
+import {SectionsStateService} from "../services/sections-state.service";
+import {OnChanges} from "@angular/core";
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss']
 })
-export class SectionComponent {
+export class SectionComponent implements OnChanges {
   @Input() form?: SectionFormGroup;
+  @Input() itemId: string;
+  displayRemoveButton: boolean;
 
-  constructor() {
+  constructor(public sectionsState: SectionsStateService
+  ) {
+    this.displayRemoveButton = false;
+    this.itemId = '';
+  }
+
+  ngOnChanges(changes:SimpleChanges) {
+    this.displayRemoveButton = this.sectionsState.getSections()[0][0] !== this.form;
   }
 }
