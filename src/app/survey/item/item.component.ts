8fde54a443235f891d4834d1cd1d0684d5ce6d4e
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-26 01:47:06 +0200

    Displaying question content only for non-grid questions

--- src/app/survey/item/item.component.ts
+++ src/app/survey/item/item.component.ts
@@ -12,15 +12,18 @@ export class ItemComponent implements OnChanges {
   @Input() item?: Item;
   control: FormControl;
   typeMatches: Map<string, boolean>;
+  displayQuestionContent: boolean;
 
   constructor(private builder: FormBuilder) {
     this.control = this.builder.control('');
     this.typeMatches = new Map();
     SHORT_ITEM_TYPES.forEach((type) => this.typeMatches.set(type, false));
+    this.displayQuestionContent = true;
   }
 
   ngOnChanges(changes: SimpleChanges) {
     this.control.setValue(this.item?.questions[0].value);
     SHORT_ITEM_TYPES.forEach((type) => this.typeMatches.set(type, new RegExp(type).test(this.item!.type)));
+    this.displayQuestionContent = !/grid/.test(this.item!.type);
   }
 }
