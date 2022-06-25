7aa2812b6334c797328bce7e17bea5b55f235cee
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-26 01:42:15 +0200

    Open questions layout implemented

--- src/app/survey/content-open/content-open.component.ts
+++ src/app/survey/content-open/content-open.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {InputType} from "../../shared/types/input-type";
+import {Item} from "../interfaces/item";
 
 @Component({
   selector: 'app-content-open',
   templateUrl: './content-open.component.html',
   styleUrls: ['./content-open.component.scss']
 })
-export class ContentOpenComponent implements OnInit {
+export class ContentOpenComponent implements OnChanges {
+  @Input() item?: Item;
+  displayAsTextarea: boolean;
+  control: FormControl;
+  inputType: InputType;
 
-  constructor() { }
+  constructor(private builder: FormBuilder) {
+    this.displayAsTextarea = false;
+    this.inputType = 'text';
+    this.control = this.builder.control('');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.item) {
+      this.displayAsTextarea = /Long/.test(this.item.type);
+      this.inputType = /Numeric/.test(this.item.type) ? 'number' : 'text';
+      if (this.item.required) {
+        this.control.setValidators(Validators.required);
+      }
+    }
   }
 
 }
