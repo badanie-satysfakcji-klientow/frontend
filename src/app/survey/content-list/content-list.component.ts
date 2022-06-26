2bfba3a882b6d35e513675310d27498d61b99cdd
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-26 02:30:32 +0200

    List questions layout

--- src/app/survey/content-list/content-list.component.ts
+++ src/app/survey/content-list/content-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, SimpleChanges} from '@angular/core';
+import {ContentComponent} from "../classes/content.component";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {DropdownItem} from "../../shared/interfaces/dropdown-item";
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent implements OnInit {
+export class ContentListComponent extends ContentComponent implements OnChanges {
+  control: FormControl
+  items!: DropdownItem[];
 
-  constructor() { }
+  constructor(private builder: FormBuilder) {
+    super();
+    this.control = this.builder.control('');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.item) {
+      if (this.item.required) {
+        this.control.setValidators(Validators.required);
+      }
+      this.items = this.item.options.map(({content, id}) => ({label: content, value: id}))
+      console.log(this.items)
+    }
   }
 
 }
