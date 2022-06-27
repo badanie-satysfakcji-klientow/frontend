a9e3496e36df3aaf22aa967e71c54acdc96786d1
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-27 21:19:09 +0200

    Paused surveys

--- src/app/survey/begin-survey/begin-survey.component.ts
+++ src/app/survey/begin-survey/begin-survey.component.ts
@@ -8,6 +8,7 @@ import {SurveyStateService} from "../services/survey-state/survey-state.service"
   styleUrls: ['./begin-survey.component.scss']
 })
 export class BeginSurveyComponent {
+  paused: boolean;
 
   constructor(private route: ActivatedRoute,
               public surveyState: SurveyStateService,
@@ -15,9 +16,10 @@ export class BeginSurveyComponent {
   ) {
     this.surveyState.set(route.snapshot.data['survey']);
     this.route.data.subscribe(({survey}) => this.surveyState.set(survey));
+    this.paused = this.route.snapshot.data['survey'].paused;
   }
 
-  onBeginClick(){
+  onBeginClick() {
     const surveyId = this.surveyState.getSurveyId();
     const firstSection = this.surveyState.nextSection();
     this.router.navigateByUrl(`/survey/${surveyId}/${firstSection}`).then(Function.prototype());
