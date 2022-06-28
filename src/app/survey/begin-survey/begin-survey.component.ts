b0a7c1b6c2f15fbd3ea2c4bb15a90764db6723c9
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-28 23:57:35 +0200

    Expired survey

--- src/app/survey/begin-survey/begin-survey.component.ts
+++ src/app/survey/begin-survey/begin-survey.component.ts
@@ -10,15 +10,18 @@ import {FullSurvey} from "../interfaces/full-survey";
 })
 export class BeginSurveyComponent {
   paused: boolean;
+  expired: boolean;
 
   constructor(private route: ActivatedRoute,
               public surveyState: SurveyStateService,
               private router: Router
   ) {
-    const snapshot: FullSurvey = route.snapshot.data['survey'] as FullSurvey
-    this.surveyState.set(snapshot);
-    this.route.data.subscribe(({survey}) => this.surveyState.set(survey));
+    const snapshot: FullSurvey = route.snapshot.data['survey'] as FullSurvey;
     this.paused = snapshot.paused;
+    this.expired = Date.parse(snapshot.expires_at) < Date.now();
+    if (!(this.paused || this.expired)){
+      this.surveyState.set(snapshot);
+    }
   }
 
   onBeginClick() {
