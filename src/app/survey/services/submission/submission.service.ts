12092f2084c1028bbd303c23521839f25eea586d
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-06-29 01:31:16 +0200

    Submit service

--- src/app/survey/services/submission/submission.service.ts
+++ src/app/survey/services/submission/submission.service.ts
@@ -2,23 +2,43 @@ import {Injectable} from '@angular/core';
 import {SubmitResponse} from "../../interfaces/submit-response";
 import {API_URL} from "../../../shared/constants/api-url";
 import {HttpClient} from "@angular/common/http";
+import {SubmissionBody} from "../../interfaces/submission-body";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubmissionService {
-  private rootUrl = `${API_URL}/api/surveys`;
-  private submissionId?: string;
+  private submissionId: string;
 
   constructor(private client: HttpClient) {
+    this.submissionId = '';
+  }
+
+  private static getTargetUrl(questionId: string) {
+    return `${API_URL}/api/questions/${questionId}/answer`;
   }
 
   createSubmission(surveyId: string, intervieweeId?: string) {
     const body = intervieweeId ? {interviewee: intervieweeId} : {};
-    return this.client.post<SubmitResponse>(`${this.rootUrl}/${surveyId}/submit`, body);
+    return this.client.post<SubmitResponse>(`${API_URL}/api/surveys/${surveyId}/submit`, body);
   }
 
   setId(id: string) {
     this.submissionId = id;
+  }
+
+  submitString(questionId: string, response: string, contentOption: boolean) {
+    const body: SubmissionBody = {submission: this.submissionId};
+    if (contentOption) {
+      body.option = response;
+    } else {
+      body.content_character = response;
+    }
+    return this.client.post(SubmissionService.getTargetUrl(questionId), body);
+  }
+
+  submitNumber(questionId: string, response: number) {
+    const body: SubmissionBody = {submission: this.submissionId, content_numeric: response};
+    return this.client.post(SubmissionService.getTargetUrl(questionId), body);
   }
 }
